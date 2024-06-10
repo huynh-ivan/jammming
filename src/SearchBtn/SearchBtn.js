e8b33fa6f02afd1c48644eea995c307f7e371c3f
iehuynh <32465307+huynh-ivan@users.noreply.github.com>
2024-06-10 11:47:18 -0400

    Return searchbtn in a div instead of a fragment

--- src/SearchBtn/SearchBtn.js
+++ src/SearchBtn/SearchBtn.js
@@ -1,11 +1,12 @@
 import React from "react";
 
 function SearchBtn() {
-  return {
-    <>
-    <button type="submit">Search</button>
-    </>
-  }
+  div
+  return (
+    <div className="SearchBtn-container">
+      <button type="submit">Search</button>
+    </div>
+  )
 }
 
 export default SearchBtn();
