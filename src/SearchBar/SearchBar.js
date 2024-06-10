571ef3e17367929b86c67a5d2774d178ce2bcaee
iehuynh <32465307+huynh-ivan@users.noreply.github.com>
2024-06-10 10:25:32 -0400

    Render a search input via SearchBar.js

---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function SearchBar() {
+  return (
+    <div>
+      <input type="search" placeholder="Search for a song!"></input>
+    </div>
+  );
+}
+
+export default SearchBar();
