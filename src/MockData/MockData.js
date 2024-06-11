47e7812e7cb17225d532990031cc872c89bb17db
iehuynh <32465307+huynh-ivan@users.noreply.github.com>
2024-06-11 11:46:59 -0400

    Create array of mock tracks for testing

---
+++ src/MockData/MockData.js
@@ -0,0 +1,109 @@
+const mockTracks = [
+  {
+    trackName: 'song #1',
+    trackArtist: 'artist #1',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #2',
+    trackArtist: 'artist #1',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #3',
+    trackArtist: 'artist #1',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #4',
+    trackArtist: 'artist #1',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #5',
+    trackArtist: 'artist #1',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #6',
+    trackArtist: 'artist #1',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #1',
+    trackArtist: 'artist #2',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #1',
+    trackArtist: 'artist #1',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #1',
+    trackArtist: 'artist #1',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #1',
+    trackArtist: 'artist #1',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #2',
+    trackArtist: 'artist #2',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #3',
+    trackArtist: 'artist #2',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #4',
+    trackArtist: 'artist #2',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #5',
+    trackArtist: 'artist #2',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #6',
+    trackArtist: 'artist #2',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #1',
+    trackArtist: 'artist #3',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #2',
+    trackArtist: 'artist #3',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #3',
+    trackArtist: 'artist #3',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #4',
+    trackArtist: 'artist #3',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #5',
+    trackArtist: 'artist #3',
+    trackAlbum: 'album #1'
+  },
+  {
+    trackName: 'song #6',
+    trackArtist: 'artist #3',
+    trackAlbum: 'album #1'
+  },
+]
+
+export { mockTracks };
