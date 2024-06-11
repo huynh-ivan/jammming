8d0b124e23a9adf6d5e1843a4467204e21bc06bb
iehuynh <32465307+huynh-ivan@users.noreply.github.com>
2024-06-11 12:16:28 -0400

    Update mockdata with actual songs and artists

--- src/MockData/MockData.js
+++ src/MockData/MockData.js
@@ -1,108 +1,111 @@
 const mockTracks = [
   {
-    trackName: 'song #1',
-    trackArtist: 'artist #1',
-    trackAlbum: 'album #1'
+    trackName: 'All You Need to Know',
+    trackArtist: 'Gryffin',
+    trackAlbum: 'Gravity',
+    key: trackArtist + 0
   },
   {
-    trackName: 'song #2',
-    trackArtist: 'artist #1',
-    trackAlbum: 'album #1'
+    trackName: 'Need Your Love',
+    trackArtist: 'Gryffin',
+    trackAlbum: 'Gravity',
+    key: trackArtist + 1
   },
   {
-    trackName: 'song #3',
-    trackArtist: 'artist #1',
-    trackAlbum: 'album #1'
+    trackName: 'Tie Me Down',
+    trackArtist: 'Gryffin',
+    trackAlbum: 'Gravity',
+    key: trackArtist + 2
   },
   {
-    trackName: 'song #4',
-    trackArtist: 'artist #1',
-    trackAlbum: 'album #1'
+    trackName: 'Nothing Like You',
+    trackArtist: 'Gryffin',
+    trackAlbum: 'Gravity',
+    key: trackArtist + 3
   },
   {
-    trackName: 'song #5',
-    trackArtist: 'artist #1',
-    trackAlbum: 'album #1'
+    trackName: 'Body Back',
+    trackArtist: 'Gryffin',
+    trackAlbum: 'Gravity',
+    key: trackArtist + 4
   },
   {
-    trackName: 'song #6',
-    trackArtist: 'artist #1',
-    trackAlbum: 'album #1'
+    trackName: 'Body Back',
+    trackArtist: 'Gryffin',
+    trackAlbum: 'Gravity',
+    key: trackArtist + 5
   },
   {
-    trackName: 'song #1',
-    trackArtist: 'artist #2',
-    trackAlbum: 'album #1'
+    trackName: 'Lose Somebody',
+    trackArtist: 'Kygo',
+    trackAlbum: 'Golden Hour',
+    key: trackArtist + 0
   },
   {
-    trackName: 'song #1',
-    trackArtist: 'artist #1',
-    trackAlbum: 'album #1'
+    trackName: 'Feels like Forever',
+    trackArtist: 'Kygo',
+    trackAlbum: 'Golden Hour',
+    key: trackArtist + 1
   },
   {
-    trackName: 'song #1',
-    trackArtist: 'artist #1',
-    trackAlbum: 'album #1'
+    trackName: 'Freedom',
+    trackArtist: 'Kygo ft. Zak Abel',
+    trackAlbum: 'Golden Hour',
+    key: trackArtist + 2
   },
   {
-    trackName: 'song #1',
-    trackArtist: 'artist #1',
-    trackAlbum: 'album #1'
+    trackName: 'Broken Glass',
+    trackArtist: 'Kygo',
+    trackAlbum: 'Golden Hour',
+    key: trackArtist + 3
   },
   {
-    trackName: 'song #2',
-    trackArtist: 'artist #2',
-    trackAlbum: 'album #1'
+    trackName: 'How would I know',
+    trackArtist: 'Kygo',
+    trackAlbum: 'Golden Hour',
+    key: trackArtist + 4
   },
   {
-    trackName: 'song #3',
-    trackArtist: 'artist #2',
-    trackAlbum: 'album #1'
+    trackName: 'Higher Love',
+    trackArtist: 'Kygo',
+    trackAlbum: 'Golden Hour',
+    key: trackArtist + 5
   },
   {
-    trackName: 'song #4',
-    trackArtist: 'artist #2',
-    trackAlbum: 'album #1'
+    trackName: 'Crawl outta love',
+    trackArtist: 'Illenium',
+    trackAlbum: 'Awake',
+    key: trackArtist + 0
   },
   {
-    trackName: 'song #5',
-    trackArtist: 'artist #2',
-    trackAlbum: 'album #1'
+    trackName: 'Needed You',
+    trackArtist: 'Illenium',
+    trackAlbum: 'Awake',
+    key: trackArtist + 1
   },
   {
-    trackName: 'song #6',
-    trackArtist: 'artist #2',
-    trackAlbum: 'album #1'
+    trackName: 'Fractures',
+    trackArtist: 'Illenium',
+    trackAlbum: 'Awake',
+    key: trackArtist + 2
   },
   {
-    trackName: 'song #1',
-    trackArtist: 'artist #3',
-    trackAlbum: 'album #1'
+    trackName: 'Leavin',
+    trackArtist: 'Illenium',
+    trackAlbum: 'Awake',
+    key: trackArtist + 3
   },
   {
-    trackName: 'song #2',
-    trackArtist: 'artist #3',
-    trackAlbum: 'album #1'
+    trackName: 'Freefall',
+    trackArtist: 'Illenium',
+    trackAlbum: 'Awake',
+    key: trackArtist + 4
   },
   {
-    trackName: 'song #3',
-    trackArtist: 'artist #3',
-    trackAlbum: 'album #1'
-  },
-  {
-    trackName: 'song #4',
-    trackArtist: 'artist #3',
-    trackAlbum: 'album #1'
-  },
-  {
-    trackName: 'song #5',
-    trackArtist: 'artist #3',
-    trackAlbum: 'album #1'
-  },
-  {
-    trackName: 'song #6',
-    trackArtist: 'artist #3',
-    trackAlbum: 'album #1'
+    trackName: 'Lost',
+    trackArtist: 'Illenium',
+    trackAlbum: 'Awake',
+    key: trackArtist + 5
   },
 ]
 
