2a3d5a9d6d079b90f875dfcd30563678a48dfe28
iehuynh <32465307+huynh-ivan@users.noreply.github.com>
2024-06-12 10:41:02 -0400

    Update key to id in mockTracks array objs

--- src/MockData/MockData.js
+++ src/MockData/MockData.js
@@ -3,109 +3,109 @@ const mockTracks = [
     trackName: 'All You Need to Know',
     trackArtist: 'Gryffin',
     trackAlbum: 'Gravity',
-    key: trackArtist + 0
+    id: trackArtist + 0
   },
   {
     trackName: 'Need Your Love',
     trackArtist: 'Gryffin',
     trackAlbum: 'Gravity',
-    key: trackArtist + 1
+    id: trackArtist + 1
   },
   {
     trackName: 'Tie Me Down',
     trackArtist: 'Gryffin',
     trackAlbum: 'Gravity',
-    key: trackArtist + 2
+    id: trackArtist + 2
   },
   {
     trackName: 'Nothing Like You',
     trackArtist: 'Gryffin',
     trackAlbum: 'Gravity',
-    key: trackArtist + 3
+    id: trackArtist + 3
   },
   {
     trackName: 'Body Back',
     trackArtist: 'Gryffin',
     trackAlbum: 'Gravity',
-    key: trackArtist + 4
+    id: trackArtist + 4
   },
   {
     trackName: 'Body Back',
     trackArtist: 'Gryffin',
     trackAlbum: 'Gravity',
-    key: trackArtist + 5
+    id: trackArtist + 5
   },
   {
     trackName: 'Lose Somebody',
     trackArtist: 'Kygo',
     trackAlbum: 'Golden Hour',
-    key: trackArtist + 0
+    id: trackArtist + 0
   },
   {
     trackName: 'Feels like Forever',
     trackArtist: 'Kygo',
     trackAlbum: 'Golden Hour',
-    key: trackArtist + 1
+    id: trackArtist + 1
   },
   {
     trackName: 'Freedom',
     trackArtist: 'Kygo ft. Zak Abel',
     trackAlbum: 'Golden Hour',
-    key: trackArtist + 2
+    id: trackArtist + 2
   },
   {
     trackName: 'Broken Glass',
     trackArtist: 'Kygo',
     trackAlbum: 'Golden Hour',
-    key: trackArtist + 3
+    id: trackArtist + 3
   },
   {
     trackName: 'How would I know',
     trackArtist: 'Kygo',
     trackAlbum: 'Golden Hour',
-    key: trackArtist + 4
+    id: trackArtist + 4
   },
   {
     trackName: 'Higher Love',
     trackArtist: 'Kygo',
     trackAlbum: 'Golden Hour',
-    key: trackArtist + 5
+    id: trackArtist + 5
   },
   {
     trackName: 'Crawl outta love',
     trackArtist: 'Illenium',
     trackAlbum: 'Awake',
-    key: trackArtist + 0
+    id: trackArtist + 0
   },
   {
     trackName: 'Needed You',
     trackArtist: 'Illenium',
     trackAlbum: 'Awake',
-    key: trackArtist + 1
+    id: trackArtist + 1
   },
   {
     trackName: 'Fractures',
     trackArtist: 'Illenium',
     trackAlbum: 'Awake',
-    key: trackArtist + 2
+    id: trackArtist + 2
   },
   {
     trackName: 'Leavin',
     trackArtist: 'Illenium',
     trackAlbum: 'Awake',
-    key: trackArtist + 3
+    id: trackArtist + 3
   },
   {
     trackName: 'Freefall',
     trackArtist: 'Illenium',
     trackAlbum: 'Awake',
-    key: trackArtist + 4
+    id: trackArtist + 4
   },
   {
     trackName: 'Lost',
     trackArtist: 'Illenium',
     trackAlbum: 'Awake',
-    key: trackArtist + 5
+    id: trackArtist + 5
   },
 ]
 
