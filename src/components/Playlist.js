0294881fdfd3c1422b924e5aba73735537e92b77
iehuynh <32465307+huynh-ivan@users.noreply.github.com>
2024-06-13 11:43:03 -0400

    Remove SaveToPlaylist button

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -2,23 +2,25 @@ import React from "react";
 import Track from "./Track";
 
 function Playlist({ playlistState, removeFromPlaylist }) {
+  // if I import Track into Playlist, it's state should inherit from the parent, 
   return (
     <div>
       <ul>
         {playlistState.map(track => {
           return (
             <Track
+              key={track.id}
               name={track.trackName}
               artist={track.trackArtist}
               album={track.album}
-              removeFromPlaylist={removeFromPlaylist}
             />
           )
         })
         }
       </ul>
+      {/* <SavePlaylistButton /> */}
     </div>
   )
 }
 
-export default Playlist();
+export default Playlist;
