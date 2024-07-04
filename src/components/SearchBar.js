505a64ad89aef97ee8947f7b4454eebd682fa9f7
iehuynh <32465307+huynh-ivan@users.noreply.github.com>
2024-07-04 13:20:43 -0400

    Move state down to SearchBar and incorporate Spotify search util

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,29 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
-function SearchBar({ type, setUserQuery }) {
-  const handleChange = (event) => {
-    console.log(event.target.value);
-    setUserQuery(event.target.value);
-  }
+function SearchBar(props) {
+  const [term, setTerm] = useState('')
+
+  const handleChange = useCallback((event) => {
+    setTerm(event.target.value)
+  })
+
+  const handleSearch = useCallback(() => {
+    props.onSearch(term);
+  }, [props.onSearch, term])
+
 
   return (
-    <input
-      type={type}
-      onChange={handleChange}
-      placeholder="Search for a song!" />
+    <div className="search-bar">
+      <input
+        type='search'
+        onChange={handleChange}
+        placeholder="Search for a song!" />
+      <button type={props.buttonType} onClick={props.handleSubmit} className="search-btn">
+        Search
+      </button>
+      <button onClick={handleSearch}>View Console</button>
+    </div>
+
   );
 }
 
