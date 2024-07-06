36ab7943f81d60f4fa80e8569a8ad17032afc15c
iehuynh <32465307+huynh-ivan@users.noreply.github.com>
2024-07-06 17:20:06 -0400

    Refactor Playlist.js to use refactored methods from app.js

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -1,17 +1,27 @@
-import React from "react";
+import React, { useCallback } from "react";
 import Tracklist from "./Tracklist";
 
 
-function Playlist({ playlist, removeFromPlaylist, setPlaylistName }) {
+function Playlist(props) {
+  const handleChange = useCallback(
+    (event) => {
+      props.onNameChange(event.target.value);
+    },
+    [props.onNameChange]
+  );
+
   return (
-    <div>
-      <input onChange={(e) => setPlaylistName(e.target.value)}>{playlist.name.value}</input>
+    <div className="Playlist">
+      <input onChange={handleChange} defaultValue={"New Playlist"} />
       <Tracklist
-        tracklist={playlist.tracklist}
-        action={removeFromPlaylist}
-        buttonInnerText='Remove' />
+        tracklist={props.playlistTracks}
+        isRemoval={true}
+        onRemov={props.onRemove} />
+      <button className="Playlist-save" onClick={props.onSave}>
+        Save to Spotify
+      </button>
     </div>
-  )
-}
+  );
+};
 
 export default Playlist;
